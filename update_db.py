7cce4b93c61a2a60cb0c3278ae369443dcf05007
Brandon Callender <b.callender@salesforce.com>
2014-08-24 19:37:26 -0700

    typo

--- update_db.py
+++ update_db.py
@@ -12,10 +12,11 @@ import postService
 from dateutil import parser 
 
 app = Flask("Crushes")
+db = mongo.posts
 
 def update_data():
 	with app.app_context():
-			last_updated = list(mongo.db.posts.find().sort([('created', -1)]).limit(1))[0]['created']
+			last_updated = list(db.find().sort([('created', -1)]).limit(1))[0]['created']
 			date = last_updated.strftime('%s')
 			postService.update_posts(date)
 
